26ad214b9cdbbb2b42439b58ebab37c9b7e85e17
amr-mohamed1 <amressa201311@gmail.com>
2022-02-15 14:30:42 +0200

    15/2/2022

---
+++ session4/js/main.js
@@ -0,0 +1,23 @@
+
+// var by_id = document.getElementById("h3_id");
+// var by_class = document.getElementsByClassName("h3_class");
+// var by_tag = document.getElementsByTagName("h3");
+// var by_tag = document.getElementsByTagName("h3");
+// var by_tag = document.querySelectorAll(".h3_class");
+// tag -> by_tag[0]
+
+// var by_class = document.getElementById("h3_id").innerHTML="<h1>asdsad</h1>";
+// var by_class = document.getElementById("h3_id").innerText="asdkjgajsdgjhsgad";
+
+
+
+// var by_class = document.getElementById("h3_id").innerHTML=document.links;
+// var by_class = document.getElementById("h3_id").textContent="<h1>asdkjksd</h1>";
+
+// console.log(document.links)
+
+
+
+var by_class = document.getElementById("h3_id");
+
+by_class.textContent= by_class.className
