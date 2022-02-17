bd245fda97092c5220a6f802516d48c4e3e47cf1
amr-mohamed1 <amressa201311@gmail.com>
2022-02-17 13:59:14 +0200

    16/2/2022

---
+++ session5/js/main.js
@@ -0,0 +1,131 @@
+// var div = document.getElementById("div");
+// // console.log(x.lastElementChild);
+
+// var x= document.createElement('p');
+// var y = document.createTextNode("hello from js");
+
+// x.appendChild(y);
+
+// div.insertBefore(x,div.firstElementChild)
+
+// ;
+
+
+// var div = document.getElementById("div");
+
+// div.removeChild(div.firstElementChild);
+
+
+// var div = document.getElementById("div");
+
+// console.log(div.childNodes[4].nodeValue )
+
+
+// var div = document.getElementById("div");
+
+// console.log(document.lastModified )
+
+// window.open("https://github.com/amr-mohamed1/blood_bank","_blank","width=300,height=500")
+// window.scrollBy(100,1000)
+// console.log(window.scrollY)
+
+
+// console.log(window.pageXOffset)
+
+
+
+
+// let x = document.getElementById("img");
+// console.log(x.getAttribute("alt"));
+// if(!x.hasAttribute("alt")){
+//     x.removeAttribute("src")
+// }
+
+// let x = document.getElementById("div");
+
+// x.classList.add("ali_askjdhaskd","php","js")
+
+// let x = document.getElementById("div");
+
+// x.classList.remove("amr","mohamed")
+
+// let y = document.getElementById("btn");
+// y.onclick=function(){
+//     x.classList.toggle("hide");
+// }
+
+
+// let x = document.getElementById("first");
+
+// console.log(x.firstElementChild);
+
+// var div = document.getElementById("first")
+// var x = document.createElement("p");
+// var y = document.createTextNode("hello from js");
+// x.appendChild(y);
+
+// div.insertBefore(x);
+
+
+// var x = document.getElementById("first");
+// var y = document.getElementById("btn");
+
+// x.removeChild(x.lastElementChild)
+
+// console.log(x.lastElementChild.tagName)
+
+// var y = x.firstElementChild.cloneNode(true);
+
+// var div2 = document.getElementById("sec");
+
+// div2.appendChild(y);
+
+// console.log(before(x.lastElementChild   ))
+
+
+// y.addEventListener("dblclick",function(){
+//     console.log("hello")
+// })
+
+
+// var x = document.getElementById("first");
+
+// x.style.color="red"
+
+// var x = document.getElementById("first");
+
+// var y = x.firstElementChild;
+
+// y.addEventListener("click",function(){
+//     window.prompt("what is your name","your name");
+// })
+
+
+// setInterval(
+// function(){
+//     console.log("hello")
+// }
+// ,1000)
+
+// window.open("https://www.ieee-bub.com/winterEvent.php","","width=400,height=500")
+
+// window.scrollBy(0,1500)
+
+// console.log(window.scrollY)
+
+
+
+// setTimeout(function , delay)
+
+var x = document.getElementById("first");
+
+var y = x.firstElementChild;
+
+
+    // history.back();
+
+    screen.height
+
+// window.close();
+console.log(screen.height)
+
